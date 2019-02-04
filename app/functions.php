1abae3fbb18c5449471dc331c7e8c479db68b171
Juha Rajamäki <juha.rajamaki@ceon.fi>
2019-02-04 22:45:44 +0000

    fixor

---
+++ app/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+class visualizer {
+
+  protected $id;
+  private $email;
+
+  public function __construct()
+	{
+  }
+
+	public function visualize ($data) {
+
+    return $data;
+
+  }
+
+}
